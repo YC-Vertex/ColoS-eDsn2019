b5219c82d019b61ee3f20ed6ae999956eb45f1e9
武钰淞 <bqsgwys@outlook.com>
2019-11-17 11:13:06 +0800

    fm

--- User/Drv/v_FMACHINE.h
+++ User/Drv/v_FMACHINE.h
@@ -1,6 +1,9 @@
 #ifndef _V_FMACHINE__H_
 #define _V_FMACHINE__H_
 #include "main.h"
+#include "v_MOTION.h"
+#include "v_EDC21.h"
+#include "v_JY61.h"
 #define FHALT 0
 #define FTARGET 1
 #define FMANLINE 2
@@ -11,17 +14,18 @@ typedef struct fNode *fnode;
 
 /**************************************
 * current:
-* 0 - FHALT
+* 0 - FHALT TIME?
 * 1 - FTARGET TARX TARY TARZ
 * 1 - FLTL R NXTX NXTY NXTZ
 * 2 - FNXTLINE XDIRC YDIRC
 * 3 - FMANLINE XSPEED YSPEED ZSPEED
-* 4 - FMANCIR R THETA SPEED
+* 4 - FMANCIR R THETA TIME
 **************************************/
 struct fNode{
-  uint16_t current,nxt;
-  uint16_t data[8];
-};  
+  uint8_t current;
+  float data[8];
+};
+extern fnode* queue;
 
 float omegaFusion(float omega1,float omega2);
 
